82ab71a81fdc594d0c961a11766119b1d15d4701
Evgenii Lutscenko <evgenlutscenko@icloud.com>
2021-05-09 16:50:23 +0300

    add first task

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(9);
+"use strict"
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели ?", "10");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+
+let a = prompt("Один из последних фильмов который вы посмотрели");
+let b = prompt("Как вы его оцените ?");
+let c = prompt("Один из последних фильмов который вы посмотрели");
+let d = prompt("Как вы его оцените ?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
